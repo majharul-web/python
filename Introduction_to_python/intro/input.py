9af38ae3dbaa5768ac9b3c24dc74ec04ae927c5d
majharul <majharuljony163@gmail.com>
2025-04-02 15:25:16 +0600

    learn input output

---
+++ Introduction_to_python/intro/input.py
@@ -0,0 +1,15 @@
+
+print("Enter your age 1 : ")
+age_1=input()
+print("Enter your age 2 : ")
+age_2=input()
+
+# by default input is string
+print(type(age_1))
+print("Your age : ",int(age_1)+int(age_2))
+
+name=input("Enter your name : ")
+print("Your name : ",name)
+# convert type
+print("convert number to string",str(10))
+print("convert number to float",float(10))
