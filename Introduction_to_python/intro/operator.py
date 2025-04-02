d0f8868fca3007929ad90bb37e5db4b79f73ab28
majharul <majharuljony163@gmail.com>
2025-04-02 15:42:15 +0600

    basic operator

---
+++ Introduction_to_python/intro/operator.py
@@ -0,0 +1,45 @@
+# Arithmatic Operator : +, -, *, /, %,
+# ** : Exponentiation
+# // : Floor division
+
+num_1=101
+num_2=15
+print(num_1+num_2)
+print(num_1-num_2)
+print(num_1*num_2)
+print(num_1/num_2)
+print(num_1%num_2)
+print(num_1**num_2)
+print(num_1//num_2)
+
+
+
+'''
+Python Comparison Operators
+Comparison operators are used to compare two values:
+
+Operator	Name	                        Example	
+==	        Equal	                        x == y	
+!=	        Not equal	                    x != y	
+>	        Greater than	                x > y	
+<	        Less than	                    x < y	
+>=	        Greater than or equal to	    x >= y	
+<=	        Less than or equal to	        x <= y	
+
+Python Logical Operators
+Operator	Description	                                             Example	
+and 	    Returns True if both statements are true	             x < 5 and  x < 10	
+or	        Returns True if one of the statements is true	         x < 5 or x < 4	
+not	        Reverse the result, returns False if the result is true	not(x < 5 and x < 10)	
+
+Python Identity Operators
+Operator	Description	                                                Example	
+is 	        Returns True if both variables are the same object	        x is y	
+is not	    Returns True if both variables are not the same object	    x is not y	
+
+Python Membership Operators
+Operator	Description	                                                                        Example
+in 	        Returns True if a sequence with the specified value is present in the object	    x in y	
+not in	    Returns True if a sequence with the specified value is not present in the object	x not in y	
+
+'''
