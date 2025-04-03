d583d392304bf521d3b1fb456c47550c7e20025e
majharul <majharuljony163@gmail.com>
2025-04-03 11:32:25 +0600

    basic set learn

---
+++ Introduction_to_python/basic_concept_3/set.py
@@ -0,0 +1,14 @@
+myset_1=set([1,2,3,4,5])
+myset_2={'apple','banana'}
+
+myset_1.add(52)
+print(myset_2,myset_1)
+for item in myset_1:
+    print(item)
+
+myset_2.remove('banana')
+for item in myset_2:
+    print(item)
+
+myset_3=myset_1.union(myset_2)
+print(myset_3)
--- Introduction_to_python/basic_concept_3/tuples.py
+++ Introduction_to_python/basic_concept_3/tuples.py
@@ -1,3 +1,6 @@
+
+# Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+
 mytuple_1=1,2,3
 mytuple_2=(1,2,3,'tuple')
 mytuple_3=tuple(('tx',True,1))
@@ -11,3 +14,5 @@ for item in mytuple_3:
 
 i_1,*all=mytuple_2
 print(i_1,all)
+
+# mytuple_1[0]=5
