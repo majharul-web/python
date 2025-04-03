ab487e16229c5a923bfae3fe71ed08b7d1df4d08
majharul <majharuljony163@gmail.com>
2025-04-03 11:15:07 +0600

    learn basic tuple

---
+++ Introduction_to_python/basic_concept_3/tuples.py
@@ -0,0 +1,13 @@
+mytuple_1=1,2,3
+mytuple_2=(1,2,3,'tuple')
+mytuple_3=tuple(('tx',True,1))
+print(mytuple_3)
+print(mytuple_3[0])
+print(mytuple_1[-1])
+print(mytuple_2[1:])
+
+for item in mytuple_3:
+    print(item)
+
+i_1,*all=mytuple_2
+print(i_1,all)
